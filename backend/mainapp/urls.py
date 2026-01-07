d335f8e9f319dd3c2c2a404a3e39a1a60a208b6c
Aarav-Jain123 <137067931+Aarav-Jain123@users.noreply.github.com>
2026-01-07 13:43:14 +0530

    Update urls.py

--- backend/mainapp/urls.py
+++ backend/mainapp/urls.py
@@ -4,4 +4,7 @@ from .views import *
 urlpatterns = [
     path('', home, name='home'),
     path('model_page/', model_page, name='model page'),
+    path("clean_dataset_page/", clean_dataset_page, name="clean dataset page"),
+    path("dirty_dataset_page/", dirty_dataset_page, name="dirty dataset page"),
 ]
+
