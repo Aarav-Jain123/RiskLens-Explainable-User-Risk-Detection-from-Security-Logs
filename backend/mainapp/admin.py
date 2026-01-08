f76bab7fb857a5adc0ab2cbfdd5ffb40a17bbe38
Aarav-Jain123 <137067931+Aarav-Jain123@users.noreply.github.com>
2026-01-08 16:46:44 +0530

    Update admin.py

--- backend/mainapp/admin.py
+++ backend/mainapp/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.site_header = "CyberSec Admin"
-admin.site.site_title = "CyberSec Admin Portal"
-admin.site.index_title = "Welcome to CyberSec Admin Portal"
+admin.site.site_header = "RiskLens Admin"
+admin.site.site_title = "RiskLens Admin Portal"
+admin.site.index_title = "Welcome to RiskLens Admin Portal"
 
 admin.site.register(ThreatReports)
